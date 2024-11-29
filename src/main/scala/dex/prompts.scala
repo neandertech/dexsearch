76fae9bbaec043610e252b4adebb66d9870ad054
Anton Sviridov <keynmol@gmail.com>
2024-11-29 09:02:15 +0000

    WIP: cue4s port

--- src/main/scala/dex/prompts.scala
+++ src/main/scala/dex/prompts.scala
@@ -6,104 +6,57 @@ import scala.scalajs.js.Promise
 import scala.scalajs.js.Thenable
 import cats.effect.IO
 import scala.scalajs.js.JSConverters._
+import cue4s.*, catseffect.PromptsIO
 
-@js.native
-@JSImport("prompts", JSImport.Namespace)
-object prompts extends js.Any:
-  def default[T](x: js.Object): Promise[T] = js.native
+def promptLibName(prompts: PromptsIO): IO[LibName] =
+  prompts
+    .io(Prompt.Input("Which library?"))
+    .map(_.toEither)
+    .flatMap(IO.fromEither)
+    .map(LibName(_))
 
-def ioPrompt[T](x: js.Object): IO[T] =
-  IO.fromPromise(IO(prompts.default(x)))
+def promptProject(prompts: PromptsIO, project: js.Array[Project]): IO[Project] =
+  val mapping = project.toList
+    .map: p =>
+      val title = s"${p.repository} from ${p.organization}"
+      val value = p
 
-def promptLibName: IO[LibName] =
-  val libNameQuestion = new js.Object {
-    val `type` = "text"
-    val name = "libName"
-    val message = "Which library ?"
-  }
+      title -> p
+    .toMap
 
-  trait LibNameAnswer extends js.Object {
-    val libName: String
-  }
-
-  ioPrompt[LibNameAnswer](libNameQuestion).map(_.libName).map(LibName(_))
-end promptLibName
-
-def promptProject(project: js.Array[Project]): IO[Project] =
-  val projectQuestion = new js.Object {
-    val `type` = "autocomplete"
-    val name = "project"
-    val message = "From which repo ?"
-    val choices = project.map { p =>
-      new js.Object {
-        val title = s"${p.repository} from ${p.organization}"
-        val value = p
-      }
-    }
-  }
-
-  trait ProjectAnswer extends js.Object {
-    val project: Project
-  }
-
-  ioPrompt[ProjectAnswer](projectQuestion).map(_.project)
+  prompts
+    .io(Prompt.SingleChoice("From which repo?", mapping.keySet.toList.sorted))
+    .map(_.toEither)
+    .flatMap(IO.fromEither)
+    .map(mapping(_))
 end promptProject
 
-def promptModules(project: Project): IO[js.Array[String]] =
-  val moduleQuestion = new js.Object {
-    val `type` = "autocompleteMultiselect"
-    val name = "modules"
-    val message = "Which modules ?"
-    val choices = project.artifacts.map { artifact =>
-      new js.Object {
-        val title = artifact
-        val value = artifact
-      }
-    }
-  }
+def promptModules(prompts: PromptsIO, project: Project): IO[js.Array[String]] =
+  val choices = project.artifacts.toList
 
-  trait ProjectAnswer extends js.Object {
-    val modules: js.Array[String]
-  }
-
-  ioPrompt[ProjectAnswer](moduleQuestion).map(_.modules)
+  prompts
+    .io(Prompt.MultipleChoice.withNoneSelected("Which modules?", choices))
+    .map(_.toEither)
+    .flatMap(IO.fromEither)
+    .map(_.toJSArray)
 end promptModules
 
-def promptVersion(project: ProjectDetails): IO[String] =
-  val versionQuestion = new js.Object {
-    val `type` = "autocomplete"
-    val name = "version"
-    val message = "Which version ?"
-    val choices = project.versions.map { version =>
-      new js.Object {
-        val title = version
-        val value = version
-      }
-    }
-  }
-
-  trait VersionAnswer extends js.Object:
-    val version: String
-
-  ioPrompt[VersionAnswer](versionQuestion).map(_.version)
+def promptVersion(prompts: PromptsIO, project: ProjectDetails): IO[String] =
+  prompts
+    .io(Prompt.SingleChoice("Which version?", project.versions.toList))
+    .map(_.toEither)
+    .flatMap(IO.fromEither)
 end promptVersion
 
-def promptBuildTool: IO[BuildTool] =
-  val versionQuestion = new js.Object {
-    val `type` = "select"
-    val name = "buildTool"
-    val message = "Which build tool ?"
-    val choices = BuildTool.values.map { bt =>
-      new js.Object {
-        val title = bt.toString()
-        val value = bt
-      }
-    }.toJSArray
-  }
-
-  trait VersionAnswer extends js.Object {
-    val buildTool: BuildTool
-  }
-
-  ioPrompt[VersionAnswer](versionQuestion).map(_.buildTool)
+def promptBuildTool(prompts: PromptsIO): IO[BuildTool] =
+  prompts
+    .io(
+      Prompt.SingleChoice(
+        "Which build tool?",
+        BuildTool.values.map(_.toString).toList
+      )
+    )
+    .map(_.toEither)
+    .flatMap(IO.fromEither)
+    .map(BuildTool.valueOf)
 end promptBuildTool
